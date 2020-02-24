e011ab7a4594b2d3497f710bb81a27838da282db
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-02-24 13:23:58 +0100

    working on camisani calzolari algorithm

--- code/camisani-calzolari_algorithm.py
+++ code/camisani-calzolari_algorithm.py
@@ -7,6 +7,7 @@ def camisani_calzolari_algorithm(users_dataset, tweets_dataset):
         users_reader = csv.reader(users_file, delimiter=',')
         next(users_reader, None)
         for row in users_reader:
+            user_id = row[0]
             name = row[1]
             followers_count = row[4]
             favorites_count = row[6]
@@ -14,6 +15,17 @@ def camisani_calzolari_algorithm(users_dataset, tweets_dataset):
             url = row[9]
             location = row[12]
             profile_image_url = row[16]
+            description = row[31]
+            with open(tweets_dataset) as tweets_file:
+                tweets_reader = csv.reader(tweets_file, delimiter=',')
+                next(tweets_reader, None)
+                    for tweet_row in tweets_reader:
+                        user_id_tweet = tweet_row[4]
+                        if user_id == user_id_tweet:
+                            source = tweet_row[3]
+                            geo = tweet_row[8]
+                            retweet_count = tweet_row[12]
+                            num_hashtags = tweet_row[15]
            
     
 
@@ -22,7 +34,8 @@ def main():
     #table 6: results of running the algorithm over the complete dataset
     dataset = '~/git/ICYBM121-p1/databases/E13/'
     users_dataset = dataset + 'users.csv'
-    camisani_calzolari_algorithm(dataset)
+    tweets_dataset = dataset + 'tweets.csv'
+    camisani_calzolari_algorithm(users_dataset, tweets_dataset)
 
 
 if __name__ == "__main__":
