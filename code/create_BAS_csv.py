bd615fae57775ffbcb180c25edb16664d7cc2d3e
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-02-29 14:06:26 +0100

    difference between dataframe and series in pandas

--- code/create_BAS_csv.py
+++ code/create_BAS_csv.py
@@ -4,7 +4,7 @@ import pandas
 
 home_directory = "/home/hanne"
 
-def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, fak_users_file, bas_users_dataset, user_id_index, header):
+def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, fak_users_file, bas_users_dataset, user_id_index, header,kind):
     copy_whole_content_of_csv(e13_dataset, bas_users_dataset, header)
     copy_whole_content_of_csv(tfp_dataset, bas_users_dataset, header)
     with open(fak_users_file, 'r', encoding = 'utf-8') as users_file:
@@ -14,26 +14,41 @@ def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, 
            user_id = row[0]
            dataset = row[1]
            if dataset == 'fsf':
-              copy_part_of_csv(fsf_dataset, bas_users_dataset, user_id, user_id_index)
+              copy_part_of_csv(fsf_dataset, bas_users_dataset, user_id, user_id_index, kind)
            elif dataset == 'int':
-              copy_part_of_csv(int_dataset, bas_users_dataset, user_id, user_id_index)
+              copy_part_of_csv(int_dataset, bas_users_dataset, user_id, user_id_index, kind)
            elif dataset == 'twt': 
-              copy_part_of_csv(twt_dataset, bas_users_dataset, user_id, user_id_index)
+              copy_part_of_csv(twt_dataset, bas_users_dataset, user_id, user_id_index, kind)
 
 
 def copy_whole_content_of_csv(dataset1, dataset2,header_csv):
     df_csv = pandas.read_csv(dataset1)
     if not os.path.isfile(dataset2):
-        df_csv.to_csv(dataset2, header=header_csv)
-    else:
-        df_csv.to_csv(dataset2, mode='a', header=False)
+        output_file = open(dataset2, 'w', encoding = 'utf-8')
+        csv_writer = csv.writer(output_file)
+        csv_writer.writerow(header_csv)
+    df_csv = pandas.read_csv(dataset1)
+    df_csv.to_csv(dataset2, mode='a', header=False)
     
 
 
-def copy_part_of_csv(dataset1, dataset2, user_id, user_id_index):
-    df_csv = pandas.read_csv(dataset1, index_col=user_id_index)
-    rows_user = df_csv.loc[int(user_id)]
-    rows_user.to_csv(dataset2, mode='a', header=False)
+def copy_part_of_csv(dataset1, dataset2, user_id, user_id_index, kind):
+    if kind == 'users':
+        df_csv = pandas.read_csv(dataset1, index_col=int(user_id_index))
+        rows_user = df_csv.loc[int(user_id)]
+        #print(len(rows_user))
+        #print(len(rows_user.columns))
+        rows_user.to_csv(dataset2, mode='a', header=False)
+    elif kind == 'tweets':
+        df_csv = pandas.read_csv(dataset1, index_col=int(user_id_index))
+        rows_user = df_csv.loc[int(user_id)]
+        if isinstance(rows_user, pd.Series):
+            rows_user.to_csv(dataset2, mode='a', header=False)
+        else:
+            for _, tweet_row in rows_user.iterrows():
+                tweet_row.to_csv(dataset2, mode='a', header=False)
+        #except:
+         #   rows_user.to_csv(dataset2, mode='a', header=False)
         
     
     
@@ -50,7 +65,7 @@ def main():
 "time_zone","location","default_profile","default_profile_image","geo_enabled","profile_image_url","profile_banner_url","profile_use_background_image",
 "profile_background_image_url_https","profile_text_color","profile_image_url_https","profile_sidebar_border_color","profile_background_tile","profile_sidebar_fill_color",
 "profile_background_image_url","profile_background_color","profile_link_color","utc_offset","protected","verified","description","updated","dataset"]
-    create_csv(e13_users_dataset, tfp_users_dataset, fsf_users_dataset, int_users_dataset, twt_users_dataset, fak_users_file, bas_users_dataset, user_id_index_users,header_users)
+    create_csv(e13_users_dataset, tfp_users_dataset, fsf_users_dataset, int_users_dataset, twt_users_dataset, fak_users_file, bas_users_dataset, user_id_index_users, header_users, 'users')
     bas_tweets_dataset = home_directory + '/git/ICYBM121-p1/code/bas_tweets.csv'
     e13_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/E13/tweets.csv'
     tfp_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/TFP/tweets.csv'
@@ -60,7 +75,7 @@ def main():
     user_id_index_tweets = 4
     header_tweets = ["created_at","id","text","source","user_id","truncated","in_reply_to_status_id","in_reply_to_user_id","in_reply_to_screen_name",
 "retweeted_status_id","geo","place","retweet_count","reply_count","favorite_count","num_hashtags","num_urls","num_mentions","timestamp"]
-    create_csv(e13_tweets_dataset, tfp_tweets_dataset, fsf_tweets_dataset, int_tweets_dataset, twt_tweets_dataset, fak_users_file, bas_tweets_dataset, user_id_index_tweets, header_tweets)
+    create_csv(e13_tweets_dataset, tfp_tweets_dataset, fsf_tweets_dataset, int_tweets_dataset, twt_tweets_dataset, fak_users_file, bas_tweets_dataset, user_id_index_tweets, header_tweets, 'tweets')
 
 
 if __name__ == "__main__":
