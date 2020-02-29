3525e209ea50d88e2bcb0054041619047c4f2b0f
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-02-29 11:04:50 +0100

    pandas to read everything without errors

--- code/create_BAS_csv.py
+++ code/create_BAS_csv.py
@@ -1,11 +1,12 @@
 import csv
-import os.path
+import os
+import pandas
 
-home_directory = '/home/hanne'
+home_directory = "/home/hanne"
 
-def create_csv(e13_dataset, ftp_dataset, fsf_dataset, int_dataset, twt_dataset, fak_users_file, bas_users_dataset, user_id_index):
+def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, fak_users_file, bas_users_dataset, user_id_index):
     copy_whole_content_of_csv(e13_dataset, bas_users_dataset)
-    copy_whole_content_of_csv(ftp_dataset, bas_users_dataset)
+    copy_whole_content_of_csv(tfp_dataset, bas_users_dataset)
     with open(fak_users_file, 'r', encoding = 'utf-8') as users_file:
         users_reader = csv.reader(users_file, delimiter=',')
         next(users_reader, None)
@@ -21,54 +22,40 @@ def create_csv(e13_dataset, ftp_dataset, fsf_dataset, int_dataset, twt_dataset, 
 
 
 def copy_whole_content_of_csv(dataset1, dataset2):
-    header_present = True
+    header_csv = ["id","name","screen_name","statuses_count","followers_count","friends_count","favourites_count","listed_count","created_at","url","lang","time_zone","location","default_profile","default_profile_image","geo_enabled","profile_image_url","profile_banner_url","profile_use_background_image","profile_background_image_url_https","profile_text_color","profile_image_url_https","profile_sidebar_border_color","profile_background_tile","profile_sidebar_fill_color","profile_background_image_url","profile_background_color","profile_link_color","utc_offset","protected","verified","description","updated","dataset"]
+    df_csv = pandas.read_csv(dataset1)
     if not os.path.isfile(dataset2):
-       header_present = False
-    output_file = open(dataset2, 'a', encoding = 'utf-8')
-    csv_writer = csv.writer(output_file)
-    with open(dataset1, 'r', encoding = 'utf-8') as users_file:
-        users_reader = csv.reader(users_file, delimiter=',')
-        if not header_present:
-            for row in users_reader:
-                csv_writer.writerow(row)
-        else:
-            next(users_reader, None)
-            for row in users_reader:
-                csv_writer.writerow(row)
-    output_file.close()
+        df_csv.to_csv(dataset2, header=header_csv)
+    else:
+        df_csv.to_csv(dataset2, mode='a', header=False)
+    
 
 
 def copy_part_of_csv(dataset1, dataset2, user_id, user_id_index):
-    output_file = open(dataset2, 'a', encoding = 'utf-8')
-    csv_writer = csv.writer(output_file)
-    with open(dataset1, 'r') as users_file:
-        users_reader = csv.reader(users_file, delimiter=',')
-        next(users_reader, None)
-        for row in users_reader:
-            if user_id == row[index]:
-                csv_writer.writerow(row)
-    output_file.close()
+    df_csv = pandas.read_csv(dataset1, index_col=user_id_index)
+    rows_user = df_csv.loc[int(user_id)]
+    rows_user.to_csv(dataset2, mode='a', header=False)
         
     
     
 def main():
     bas_users_dataset = home_directory + '/git/ICYBM121-p1/code/bas_users.csv'
     e13_users_dataset =  home_directory + '/git/ICYBM121-p1/database/E13/users.csv'
-    ftp_users_dataset =  home_directory + '/git/ICYBM121-p1/database/FTP/users.csv'
+    tfp_users_dataset =  home_directory + '/git/ICYBM121-p1/database/TFP/users.csv'
     fsf_users_dataset =  home_directory + '/git/ICYBM121-p1/database/FSF/users.csv'
     int_users_dataset =  home_directory + '/git/ICYBM121-p1/database/INT/users.csv'
     twt_users_dataset =  home_directory + '/git/ICYBM121-p1/database/TWT/users.csv'
     fak_users_file = home_directory + '/git/ICYBM121-p1/code/FAK_user_ids.csv'
     user_id_index_users = 0
-    create_csv(e13_users_dataset, ftp_users_dataset, fsf_users_dataset, int_users_dataset, twt_users_dataset, fak_users_file, bas_users_dataset, user_id_index_users)
+    create_csv(e13_users_dataset, tfp_users_dataset, fsf_users_dataset, int_users_dataset, twt_users_dataset, fak_users_file, bas_users_dataset, user_id_index_users)
     bas_tweets_dataset = home_directory + '/git/ICYBM121-p1/code/bas_tweets.csv'
     e13_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/E13/tweets.csv'
-    ftp_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/FTP/tweets.csv'
+    tfp_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/TFP/tweets.csv'
     fsf_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/FSF/tweets.csv'
     int_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/INT/tweets.csv'
     twt_tweets_dataset =  home_directory + '/git/ICYBM121-p1/database/TWT/tweets.csv'
     user_id_index_tweets = 4
-    create_csv(e13_tweets_dataset, ftp_tweets_dataset, fsf_tweets_dataset, int_tweets_dataset, twt_tweets_dataset, fak_users_file, bas_tweets_dataset, user_id_index_tweets)
+    create_csv(e13_tweets_dataset, tfp_tweets_dataset, fsf_tweets_dataset, int_tweets_dataset, twt_tweets_dataset, fak_users_file, bas_tweets_dataset, user_id_index_tweets)
 
 
 if __name__ == "__main__":
