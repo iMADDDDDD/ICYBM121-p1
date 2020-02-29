32db47bbe755f001891827655db43b0388aebace
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-02-29 16:28:07 +0100

    still working on creating BAS

--- code/create_BAS_csv.py
+++ code/create_BAS_csv.py
@@ -5,8 +5,9 @@ import pandas
 home_directory = "/home/hanne"
 
 def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, fak_users_file, bas_users_dataset, user_id_index, header,kind):
-    copy_whole_content_of_csv(e13_dataset, bas_users_dataset, header)
-    copy_whole_content_of_csv(tfp_dataset, bas_users_dataset, header)
+    key_error = 0
+    copy_whole_content_of_csv(e13_dataset, bas_users_dataset, header, user_id_index)
+    copy_whole_content_of_csv(tfp_dataset, bas_users_dataset, header, user_id_index)
     with open(fak_users_file, 'r', encoding = 'utf-8') as users_file:
         users_reader = csv.reader(users_file, delimiter=',')
         next(users_reader, None)
@@ -14,15 +15,17 @@ def create_csv(e13_dataset, tfp_dataset, fsf_dataset, int_dataset, twt_dataset, 
            user_id = row[0]
            dataset = row[1]
            if dataset == 'fsf':
-              copy_part_of_csv(fsf_dataset, bas_users_dataset, user_id, user_id_index, kind)
+              key_error = copy_part_of_csv(fsf_dataset, bas_users_dataset, user_id, user_id_index, kind,key_error)
            elif dataset == 'int':
-              copy_part_of_csv(int_dataset, bas_users_dataset, user_id, user_id_index, kind)
+              key_error = copy_part_of_csv(int_dataset, bas_users_dataset, user_id, user_id_index, kind,key_error)
            elif dataset == 'twt': 
-              copy_part_of_csv(twt_dataset, bas_users_dataset, user_id, user_id_index, kind)
+              key_error = copy_part_of_csv(twt_dataset, bas_users_dataset, user_id, user_id_index, kind, key_error)
+    print("users not found:")
+    print(key_error)
 
 
-def copy_whole_content_of_csv(dataset1, dataset2,header_csv):
-    df_csv = pandas.read_csv(dataset1)
+def copy_whole_content_of_csv(dataset1, dataset2,header_csv,user_id_index):
+    df_csv = pandas.read_csv(dataset1,index_col=int(user_id_index))
     if not os.path.isfile(dataset2):
         output_file = open(dataset2, 'w', encoding = 'utf-8')
         csv_writer = csv.writer(output_file)
@@ -32,23 +35,31 @@ def copy_whole_content_of_csv(dataset1, dataset2,header_csv):
     
 
 
-def copy_part_of_csv(dataset1, dataset2, user_id, user_id_index, kind):
+def copy_part_of_csv(dataset1, dataset2, user_id, user_id_index, kind,key_error):
     if kind == 'users':
         df_csv = pandas.read_csv(dataset1, index_col=int(user_id_index))
         rows_user = df_csv.loc[int(user_id)]
-        #print(len(rows_user))
-        #print(len(rows_user.columns))
-        rows_user.to_csv(dataset2, mode='a', header=False)
+        rows_user['user_id'] = int(user_id)
+        df_user = pandas.DataFrame(rows_user).T
+        df_user.to_csv(dataset2, index = False, mode='a', header=False)
     elif kind == 'tweets':
         df_csv = pandas.read_csv(dataset1, index_col=int(user_id_index))
-        rows_user = df_csv.loc[int(user_id)]
-        if isinstance(rows_user, pd.Series):
-            rows_user.to_csv(dataset2, mode='a', header=False)
+        try:
+            rows_user = df_csv.loc[int(user_id)]
+        except: 
+            key_error += 1
         else:
-            for _, tweet_row in rows_user.iterrows():
-                tweet_row.to_csv(dataset2, mode='a', header=False)
-        #except:
-         #   rows_user.to_csv(dataset2, mode='a', header=False)
+            if isinstance(rows_user, pandas.Series):
+                rows_user['user_id'] = int(user_id)
+                df_user = pandas.DataFrame(rows_user).T
+                df_user.to_csv(dataset2, index = False, mode='a', header=False)
+            elif isinstance(rows_user, pandas.DataFrame):
+                for _, tweet_row in rows_user.iterrows():
+                    tweet_row['user_id'] = int(user_id)
+                    tweet_row.to_csv(dataset2, mode='a', header=False)
+            else:
+                print("ERROR")
+    return key_error      
         
     
     
@@ -76,6 +87,7 @@ def main():
     header_tweets = ["created_at","id","text","source","user_id","truncated","in_reply_to_status_id","in_reply_to_user_id","in_reply_to_screen_name",
 "retweeted_status_id","geo","place","retweet_count","reply_count","favorite_count","num_hashtags","num_urls","num_mentions","timestamp"]
     create_csv(e13_tweets_dataset, tfp_tweets_dataset, fsf_tweets_dataset, int_tweets_dataset, twt_tweets_dataset, fak_users_file, bas_tweets_dataset, user_id_index_tweets, header_tweets, 'tweets')
+    
 
 
 if __name__ == "__main__":
