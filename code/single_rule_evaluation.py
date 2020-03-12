94de3ef44a5ede0250520d6982c78bec96c7b2f5
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-03-12 11:08:25 +0100

    csv files for state of search and socialbakers for table 14

--- code/single_rule_evaluation.py
+++ code/single_rule_evaluation.py
@@ -3,6 +3,9 @@ import csv
 import pandas
 import math
 import sys
+from datetime import datetime
+import pytz
+import time
 
 home_directory = "/home/hanne"
 
@@ -252,7 +255,7 @@ def van_den_beld_rules(classification_file, users_dataset, tweets_dataset, rule_
 
             elif rule_number == 4:
                 user_profile_image_url = row['profile_image_url']
-                number_same_images = df.loc[df.profile_image_url == user_profile_image_url, 'profile_image_url'].count()
+                number_same_images = df_csv.loc[df_csv.profile_image_url == user_profile_image_url, 'profile_image_url'].count()
                 if number_same_images > 1:
                     classification = 'bot'
                 else:
@@ -299,18 +302,92 @@ def check_vdb_tweet_rule(tweets_dataset, rule_number, user_id):
             if rule_number == 3:
                 text = tweet_row['text']
                 in_reply_to_user_id = tweet_row['in_reply_to_user_id']
+              
                 
 
-            #elif rule_number == 5:
+def socialbakers_rules(classification_file, users_dataset, tweets_dataset, rule_number):
+    human = 0
+    bot = 0
+    neutral = 0
+    if not os.path.isfile(classification_file):
+        with open(classification_file, mode = 'w') as output:
+            output_writer = csv.writer(output)
+            output_writer.writerow(['id','dataset', 'classification'])
+    df_csv = pandas.read_csv(users_dataset, sep=',')
+    for _, row in df_csv.iterrows():
+        #meaning of user fields
+        #https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object
+        user_already_classified = False
+        user_id = row['id']
+        dataset = row['dataset']
+       
+        with open(classification_file) as class_file:
+            class_reader = csv.reader(class_file, delimiter=',')
+            next(class_reader, None)
+            for class_row in class_reader:
+                class_user_id = class_row[0]
+                if int(user_id) == int(class_user_id):
+                    user_already_classified = True
+
+        if not user_already_classified:
+            classification = 'error'
+            if rule_number == 1:
+                followers_count = row['followers_count']
+                friends_count = row['friends_count']
+                if friends_count >= followers_count * 50:
+                    classification = 'bot'
+                else: 
+                    classification = 'human'
+
+            elif rule_number == 7:
+                created_at = row['created_at']
+                created_date = datetime.strptime(created_at,'%a %b %d %H:%M:%S %z %Y')
+
+                updated = row['updated']
+                updated_date = datetime.strptime(updated,'%Y-%m-%d %H:%M:%S')
+                timezone = pytz.timezone("Europe/Rome")
+                crawled_date = timezone.localize(updated_date)
+
+                difference = int((crawled_date - created_date).days)
+                if difference > 62:
+                    default_profile_image = row['default_profile_image']
+                    if default_profile_image == 1.0:
+                        classification = 'bot'
+                    else:
+                        classification = 'human'
+                else:
+                    classification = 'human'
+            
+            elif rule_number == 8:
+                description = row['description']
+                if isinstance(description,float) and math.isnan(description):
+                    location = row['location']
+                    if isinstance(location,float) and math.isnan(location): 
+                        friends_count = int(row['friends_count'])
+                        if friends_count > 100:
+                            classification = 'bot'
+                        else:
+                            classification = 'human'
+                    else:
+                        classification = 'human'
+                else:
+                    classification = 'human'
+                      
+
+            with open(classification_file, mode = 'a') as output:
+                output_writer = csv.writer(output)
+                output_writer.writerow([user_id, dataset, classification])
+
 
 def main():
     rule_nr = sys.argv[1]
     dataset = home_directory + '/git/ICYBM121-p1/code/'
-    classification_file = dataset + '/bas_classification_cc_r' + rule_nr + '.csv'
+    classification_file = dataset + '/bas_classification_sb_r' + rule_nr + '.csv'
     users_dataset = dataset + '/bas_users.csv'
     tweets_dataset = dataset + '/bas_tweets.csv'
-    camisani_calzolari_rules(classification_file, users_dataset, tweets_dataset, int(rule_nr))
+    #camisani_calzolari_rules(classification_file, users_dataset, tweets_dataset, int(rule_nr))
     #van_den_beld_rules(classification_file, users_dataset, tweets_dataset, int(rule_nr))
+    socialbakers_rules(classification_file, users_dataset, tweets_dataset, int(rule_nr))
 
 
 if __name__ == "__main__":
