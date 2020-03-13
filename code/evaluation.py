a9c3a6f983e67b50e9458fa37ce44c88745aacb0
hannepeeters <15998372+HannePeeters@users.noreply.github.com>
2020-03-13 18:54:31 +0100

    working on I gain *

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,6 +1,7 @@
 import math
 import csv
 import sys
+import pandas
 
 
 home_directory = '/home/hanne'
@@ -73,12 +74,67 @@ def calculate_information_gain(true_positive, true_negative, false_positive, fal
     information_gain = 1 - ((humans/total)*((-(true_negative/humans)*math.log2(true_negative/humans)) - ((false_negative/humans)*math.log2(false_negative/humans))) + (bots/total)*((-(true_positive/bots)*math.log2(true_positive/bots)) - ((false_positive/bots)*math.log2(false_positive/bots))))
     return information_gain
 
-def calculate_pearson_correlation_coefficient():
+def calculate_information_gain_star(dataset, classification_file, attribute):
+    whole_total = 3900
+    df_csv = pandas.read_csv(dataset)
+    dict_entropy_classes = {}
+    for _, row in df_csv.iterrows():
+        attribute_value = row[attribute]
+        if attribute_value not in dict_entropy_classes:
+            dict_entropy_classes[attribute_value] = [0,0,0,0]
+        user_id = row['id']
+        with open(classification_file) as class_file:
+            class_reader = csv.reader(class_file, delimiter=',')
+            next(class_reader, None)
+            for class_row in class_reader:
+                if user_id == class_row[0]:
+                    dataset = class_row[1]
+                    classification = class_row[2]
+                    if classification == 'human':
+                        if dataset == 'E13' or dataset == 'TFP':
+                            # TRUE NEGATIVE
+                            dict_entropy_classes[attribute_value][1] += 1
+                        elif dataset == 'FSF' or dataset == 'TWT' or dataset == 'INT':
+                            # FALSE NEGATIVE
+                            dict_entropy_classes[attribute_value][3] += 1
+                    else:
+                        if dataset == 'E13' or dataset == 'TFP':
+                            # FALSE POSITIVE
+                            dict_entropy_classes[attribute_value][2] += 1
+                        elif dataset == 'FSF' or dataset == 'TWT' or dataset == 'INT':
+                            # TRUE POSITIVE
+                            dict_entropy_classes[attribute_value][0] += 1
+                        
+    information_gain_star = 1
+    for value in dict_entropy_classes:
+        tp = dict_entropy_classes[value][0]
+        tn = dict_entropy_classes[value][1]
+        fp = dict_entropy_classes[value][2]
+        fn = dict_entropy_classes[value][3]
+        humans = tn + fn
+        bots = tp + fp
+        total = humans + bots
+        attribute_value_entropy = - (((humans + bots)/whole_total) * ((-(humans/total) * math.log2(humans/total)) - ((bots/total)*math.log2(bots/total))))
+        information_gain_star += attribute_value_entropy
+        
+    return information_gain_star
+
+def calculate_pearson_correlation_coefficient(classification_file):
     return 0
 
+def calculate_pearson_correlation_coefficient_star():
+    return 0
+
+
 def main():
+    dataset = home_directory + '/git/ICYBM121-p1/code'
     file_name = sys.argv[1]
-    dataset = home_directory + '/git/ICYBM121-p1/code/'
+    kind_dataset = sys.argv[2]
+    attribute = sys.argv[3]
+    if kind_dataset == 'u':
+        bas_dataset = dataset + '/' + 'bas_users.csv'
+    elif kind_dataset == 't':
+        bas_dataset = dataset + '/' + 'bas_tweets.csv'
     classification_file = dataset + '/' + file_name
     tp, tn, fp, fn = determine_positive_and_negative(classification_file)
     accuracy = calculate_accuracy(tp, tn, fp, fn)
@@ -99,6 +155,10 @@ def main():
     information_gain = calculate_information_gain(tp, tn, fp, fn)
     print("INFORMATION GAIN")
     print(information_gain)
+    information_gain_star = calculate_information_gain_star(bas_dataset, classification_file, attribute)
+    print("INFORMATION GAIN STAR")
+    print(information_gain_star)
+    
     
 
 
